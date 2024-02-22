f466681c1a1b193ed2df5c424495d178a4eb4134
VLstack <github@vlstack.fr>
2024-02-22 14:50:30 +0100

    initial commit

--- Sources/VLToggleStyleKit/VLToggleStyleKit.swift
+++ Sources/VLToggleStyleKit/VLToggleStyleKit.swift
@@ -1,2 +1,31 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import SwiftUI
+
+public
+struct VLCheckRoundToggleStyle: ToggleStyle
+{
+ public
+ func makeBody(configuration: Configuration) -> some View
+ {
+  Button { configuration.isOn.toggle() }
+  label:
+  {
+   HStack
+   {
+    configuration.label
+     .frame(maxWidth: .infinity, alignment: .leading)
+
+    Image(systemName: configuration.isOn ? "checkmark.circle.fill" : "circle")
+     .foregroundStyle(configuration.isOn ? Color.accentColor : .secondary)
+     .accessibility(label: Text(configuration.isOn ? "Checked" : "Unchecked"))
+     .imageScale(.large)
+   }
+  }
+  .buttonStyle(.plain)
+ }
+}
+
+public
+extension ToggleStyle where Self == VLCheckRoundToggleStyle
+{
+ static var checkRound: VLCheckRoundToggleStyle { .init() }
+}
